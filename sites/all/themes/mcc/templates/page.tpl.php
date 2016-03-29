001dde151b5a1334568b424afeb0d2ffac2609cb
Jennya Borodina <Jennya Borodina>
2016-03-29 12:58:18 -0400

    First commit

---
+++ sites/all/themes/mcc/templates/page.tpl.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
+
+<div id="page">
+
+  <header class="header" id="header" role="banner">
+
+    <?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+    <?php endif; ?>
+
+
+
+    <?php print render($page['header']); ?>
+
+
+
+  </header>
+
+  <div id="main">
+    <div id="navigation">
+
+      <?php print render($page['navigation']); ?>
+
+    </div>
+      <div class="highlighted">
+        <?php print render($page['highlighted']); ?>
+      </div>
+
+    <div id="content" class="column" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+
+
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div>
+
+
+
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar_first  = render($page['sidebar_first']);
+      $sidebar_second = render($page['sidebar_second']);
+    ?>
+
+    <?php if ($sidebar_first || $sidebar_second): ?>
+      <aside class="sidebars">
+        <?php print $sidebar_first; ?>
+        <?php print $sidebar_second; ?>
+      </aside>
+    <?php endif; ?>
+
+  </div>
+
+  <?php print render($page['footer']); ?>
+
+</div>
+
+<?php print render($page['bottom']); ?>
